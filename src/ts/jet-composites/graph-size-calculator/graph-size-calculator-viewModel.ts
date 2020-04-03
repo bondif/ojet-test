9341b8588774354d6afda177e627af2e8a7cca70
BOUKHLIF Mohamed <boukhlifmohamed13@gmail.com>
2020-04-03 11:01:28 +0100

    add onSave action

--- src/ts/jet-composites/graph-size-calculator/graph-size-calculator-viewModel.ts
+++ src/ts/jet-composites/graph-size-calculator/graph-size-calculator-viewModel.ts
@@ -4,11 +4,14 @@ import * as ko from "knockout";
 import componentStrings = require("ojL10n!./resources/nls/graph-size-calculator-strings");
 import Context = require("ojs/ojcontext");
 import Composite = require("ojs/ojcomposite");
+import * as HtmlUtils from 'ojs/ojhtmlutils';
 import "ojs/ojknockout";
 import "ojs/ojformlayout";
 import "ojs/ojinputtext";
 import "ojs/ojbutton";
 import "ojs/ojlabel";
+import "ojs/ojbinddom";
+import {ojBindDom} from "ojs/ojbinddom";
 
 export default class ViewModel implements Composite.ViewModel<Composite.PropertiesType> {
     busyResolve: (() => void);
@@ -16,6 +19,9 @@ export default class ViewModel implements Composite.ViewModel<Composite.Properti
     messageText: ko.Observable<string>;
     properties: Composite.PropertiesType;
     res: { [key: string]: string };
+
+    originalGraphConfig: ko.Observable<string>;
+    domBindingConfig: ojBindDom.Config<any>;
 
     constructor(context: Composite.ViewModelContext<Composite.PropertiesType>) {        
         //At the start of your viewModel constructor
@@ -28,8 +34,14 @@ export default class ViewModel implements Composite.ViewModel<Composite.Properti
 
         //Example observable
         this.messageText = ko.observable("Hello from graph-size-calculator");
+        this.originalGraphConfig = ko.observable("Hello");
         this.properties = context.properties;
         this.res = componentStrings["graph-size-calculator"];
+        this.domBindingConfig = {
+            view: HtmlUtils.stringToNodeArray(""),
+            data: {
+            }
+        };
 
         // Example for parsing context properties
         // if (context.properties.name) {
@@ -38,6 +50,24 @@ export default class ViewModel implements Composite.ViewModel<Composite.Properti
 
         //Once all startup and async activities have finished, relocate if there are any async activities
         this.busyResolve(); 
+    }
+
+    onSave(event: Event, data: ViewModel, context: Composite.ViewModelContext<Composite.PropertiesType>) {
+        document.getElementById("graphConfigLoader").classList.toggle("hide");
+
+        data.renderCalculator(data);
+    }
+
+    private renderCalculator(data: ViewModel) {
+        let wrapper = document.getElementById("wrapper");
+
+        let viewStr: string = '<oj-label for="[[inputId1]]">oj-input-text element</oj-label>';
+        data.domBindingConfig = {
+            view: HtmlUtils.stringToNodeArray(viewStr),
+            data: {
+                inputId1: 'text-input1',
+            }
+        };
     }
 
     //Lifecycle methods - implement if necessary 
